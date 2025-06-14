715eeba80fcd70dfdf7ec866c8b38cc68f49109a
sahuv-aus <59729081+sahuv-aus@users.noreply.github.com>
2025-06-14 23:04:01 +1000

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+#New Section to display Smoothies Fruit Nutrition information.
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise your Smoothie!:cup_with_straw:")
@@ -37,6 +39,9 @@ if (ingredients_list):
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -49,9 +54,6 @@ if (ingredients_list):
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-#New Section to display Smoothies Fruit Nutrition information.
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 

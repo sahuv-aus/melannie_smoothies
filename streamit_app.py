aa71234e6dc6a059653282eaacc5dfe6dd3735a6
sahuv-aus <59729081+sahuv-aus@users.noreply.github.com>
2025-06-14 22:59:12 +1000

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -52,5 +52,6 @@ if (ingredients_list):
 #New Section to display Smoothies Fruit Nutrition information.
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.datafame(data=smoothiefroot_response.json(),use_Container_width = True)
 
